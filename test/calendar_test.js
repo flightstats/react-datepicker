b7369d9fecc866ccc2f8ed3bc61daaf19e5a05b6
Egan Tamashiro <etamashiro@conducivetech.com>
2017-05-22 10:40:24 -0700

    un-xit out most tests in calendar_test

--- test/calendar_test.js
+++ test/calendar_test.js
@@ -22,13 +22,13 @@ describe('Calendar', function () {
     )
   }
 
-  xit('should start with the current date in view if no date range', function () {
+  it('should start with the current date in view if no date range', function () {
     var now = moment()
     var calendar = getCalendar()
     assert(calendar.state().date.isSame(now, 'day'))
   })
 
-  xit('should start with the today date with specified time zone', function () {
+  it('should start with the today date with specified time zone', function () {
     var utcOffset = 12
     var calendar = getCalendar({utcOffset})
     assert(calendar.state().date.isSame(moment.utc().utcOffset(utcOffset), 'day'))
@@ -47,7 +47,7 @@ describe('Calendar', function () {
     assert(calendar.state().date.isSame(selected, 'day'))
   })
 
-  xit('should start with the current date in view if in date range', function () {
+  it('should start with the current date in view if in date range', function () {
     var now = moment()
     var minDate = now.clone().subtract(1, 'year')
     var maxDate = now.clone().add(1, 'year')
@@ -55,51 +55,51 @@ describe('Calendar', function () {
     assert(calendar.state().date.isSame(now, 'day'))
   })
 
-  xit('should start with the min date in view if after the current date', function () {
+  it('should start with the min date in view if after the current date', function () {
     var minDate = moment().add(1, 'year')
     var calendar = getCalendar({ minDate })
     assert(calendar.state().date.isSame(minDate, 'day'))
   })
 
-  xit('should start with the min include date in view if after the current date', function () {
+  it('should start with the min include date in view if after the current date', function () {
     var minDate = moment().add(1, 'year')
     var calendar = getCalendar({ includeDates: [minDate] })
     assert(calendar.state().date.isSame(minDate, 'day'))
   })
 
-  xit('should start with the max date in view if before the current date', function () {
+  it('should start with the max date in view if before the current date', function () {
     var maxDate = moment().subtract(1, 'year')
     var calendar = getCalendar({ maxDate })
     assert(calendar.state().date.isSame(maxDate, 'day'))
   })
 
-  xit('should start with the max include date in view if before the current date', function () {
+  it('should start with the max include date in view if before the current date', function () {
     var maxDate = moment().subtract(1, 'year')
     var calendar = getCalendar({ includeDates: [maxDate] })
     assert(calendar.state().date.isSame(maxDate, 'day'))
   })
 
-  xit('should start with the open to date in view if given and no selected/min/max dates given', function () {
+  it('should start with the open to date in view if given and no selected/min/max dates given', function () {
     var openToDate = moment('09/28/1993', 'MM/DD/YYYY')
     var calendar = getCalendar({ openToDate })
     assert(calendar.state().date.isSame(openToDate, 'day'))
   })
 
-  xit('should start with the open to date in view if given and after a min date', function () {
+  it('should start with the open to date in view if given and after a min date', function () {
     var openToDate = moment('09/28/1993', 'MM/DD/YYYY')
     var minDate = moment('01/01/1993', 'MM/DD/YYYY')
     var calendar = getCalendar({ openToDate, minDate })
     assert(calendar.state().date.isSame(openToDate, 'day'))
   })
 
-  xit('should start with the open to date in view if given and before a max date', function () {
+  it('should start with the open to date in view if given and before a max date', function () {
     var openToDate = moment('09/28/1993', 'MM/DD/YYYY')
     var maxDate = moment('12/31/1993', 'MM/DD/YYYY')
     var calendar = getCalendar({ openToDate, maxDate })
     assert(calendar.state().date.isSame(openToDate, 'day'))
   })
 
-  xit('should start with the open to date in view if given and in range of the min/max dates', function () {
+  it('should start with the open to date in view if given and in range of the min/max dates', function () {
     var openToDate = moment('09/28/1993', 'MM/DD/YYYY')
     var minDate = moment('01/01/1993', 'MM/DD/YYYY')
     var maxDate = moment('12/31/1993', 'MM/DD/YYYY')
@@ -124,57 +124,57 @@ describe('Calendar', function () {
     assert(calendar.state().date.isSame(oneMonthFromOpenToDate))
   })
 
-  xit('should not show the year dropdown menu by default', function () {
+  it('should not show the year dropdown menu by default', function () {
     var calendar = getCalendar()
     var yearReadView = calendar.find(YearDropdown)
     expect(yearReadView).to.have.length(0)
   })
 
-  xit('should show the year dropdown menu if toggled on', function () {
+  it('should show the year dropdown menu if toggled on', function () {
     var calendar = getCalendar({ showYearDropdown: true })
     var yearReadView = calendar.find(YearDropdown)
     expect(yearReadView).to.have.length(1)
   })
 
-  xit('should show month navigation if toggled on', function () {
+  it('should show month navigation if toggled on', function () {
     var calendar = getCalendar({ includeDates: [moment()], forceShowMonthNavigation: true })
     var nextNavigationButton = calendar.find('.react-datepicker__navigation--next')
     expect(nextNavigationButton).to.have.length(1)
   })
 
-  xit('should not show the month dropdown menu by default', function () {
+  it('should not show the month dropdown menu by default', function () {
     var calendar = getCalendar()
     var monthReadView = calendar.find(MonthDropdown)
     expect(monthReadView).to.have.length(0)
   })
 
-  xit('should show the month dropdown menu if toggled on', function () {
+  it('should show the month dropdown menu if toggled on', function () {
     var calendar = getCalendar({ showMonthDropdown: true })
     var monthReadView = calendar.find(MonthDropdown)
     expect(monthReadView).to.have.length(1)
   })
 
-  xit('should not show the today button by default', function () {
+  it('should not show the today button by default', function () {
     var calendar = getCalendar()
     var todayButton = calendar.find('.react-datepicker__today-button')
     expect(todayButton).to.have.length(0)
   })
 
-  xit('should show the today button if toggled on', function () {
+  it('should show the today button if toggled on', function () {
     var calendar = getCalendar({ todayButton: 'Vandaag' })
     var todayButton = calendar.find('.react-datepicker__today-button')
     expect(todayButton).to.have.length(1)
     expect(todayButton.text()).to.equal('Vandaag')
   })
 
-  xit('should set the date when pressing todayButton', () => {
+  it('should set the date when pressing todayButton', () => {
     var calendar = getCalendar({ todayButton: 'Vandaag' })
     var todayButton = calendar.find('.react-datepicker__today-button')
     todayButton.simulate('click')
     expect(calendar.state().date.isSame(moment(), 'day'))
   })
 
-  xit('should set custom today date when pressing todayButton', () => {
+  it('should set custom today date when pressing todayButton', () => {
     var todayInAuckland = moment.utc().utcOffset(12)
     var calendar = getCalendar({ todayButton: 'Vandaag', utcOffset: 12 })
     var todayButton = calendar.find('.react-datepicker__today-button')
@@ -182,7 +182,7 @@ describe('Calendar', function () {
     expect(calendar.state().date.isSame(todayInAuckland, 'day'))
   })
 
-  xit('should track the currently hovered day', () => {
+  it('should track the currently hovered day', () => {
     const calendar = mount(
       <Calendar
           dateFormat={dateFormat}
@@ -197,7 +197,7 @@ describe('Calendar', function () {
     expect(month.prop('selectingDate').isSame(day.prop('day'), 'day')).to.be.true
   })
 
-  xit('should clear the hovered day when the mouse leaves', () => {
+  it('should clear the hovered day when the mouse leaves', () => {
     const calendar = mount(
       <Calendar
           dateFormat={dateFormat}
@@ -232,21 +232,21 @@ describe('Calendar', function () {
       )
     })
 
-    xit('calls onMonthChange prop when previous month button clicked', () => {
+    it('calls onMonthChange prop when previous month button clicked', () => {
       var select = calendar.find('.react-datepicker__navigation--previous')
       select.simulate('click')
 
       assert(onMonthChangeSpy.called === true, 'onMonthChange should be called')
     })
 
-    xit('calls onMonthChange prop when next month button clicked', () => {
+    it('calls onMonthChange prop when next month button clicked', () => {
       var select = calendar.find('.react-datepicker__navigation--next')
       select.simulate('click')
 
       assert(onMonthChangeSpy.called === true, 'onMonthChange should be called')
     })
 
-    xit('calls onMonthChange prop when month changed from month dropdown', () => {
+    it('calls onMonthChange prop when month changed from month dropdown', () => {
       var select = calendar.find(MonthDropdown).find('select')
       select.simulate('change')
 
@@ -273,21 +273,21 @@ describe('Calendar', function () {
       )
     })
 
-    xit('calls onDropdownFocus prop when year select is focused', () => {
+    it('calls onDropdownFocus prop when year select is focused', () => {
       var select = calendar.find('.react-datepicker__year-select')
       select.simulate('focus')
 
       assert(onDropdownFocusSpy.called === true, 'onDropdownFocus should be called')
     })
 
-    xit('calls onDropdownFocus prop when month select is focused', () => {
+    it('calls onDropdownFocus prop when month select is focused', () => {
       var select = calendar.find('.react-datepicker__month-select')
       select.simulate('focus')
 
       assert(onDropdownFocusSpy.called === true, 'onDropdownFocus should to be called')
     })
 
-    xit('does not call onDropdownFocus prop when the dropdown container div is focused', () => {
+    it('does not call onDropdownFocus prop when the dropdown container div is focused', () => {
       var select = calendar.find('.react-datepicker__header__dropdown')
       select.simulate('focus')
 
